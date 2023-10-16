74f4743aaa91bea76a4c5d87b3792e83ac89882b
aeo <47936846+aeoniumsalad@users.noreply.github.com>
2023-10-16 11:55:00 -0700

    Update functionalityV1.2.js

--- basicdark/functionalityV1.js
+++ basicdark/functionalityV1.js
@@ -1,3 +1,6 @@
-  window.onload = function() {
-  document.getElementById('printOG').innerHTML = '&#x75;&#x72;&#x6c;&#x20;&#x73;&#x68;&#x6f;&#x75;&#x6c;&#x64;&#x20;&#x62;&#x65;&#x20;<u>&#x2f;&#x61;&#x65;&#x6f;&#x2d;&#x66;&#x61;&#x6e;&#x63;&#x79;&#x63;&#x61;&#x74;</u>&#x2e;';
-  };
+window.onload = function() {
+  const targetDiv = document.querySelector('.rightColumn div.rightFoot');
+  if (targetDiv) {
+    targetDiv.innerHTML = '&#x75;&#x72;&#x6c;&#x20;&#x73;&#x68;&#x6f;&#x75;&#x6c;&#x64;&#x20;&#x62;&#x65;&#x20;<u>&#x2f;&#x61;&#x65;&#x6f;&#x2d;&#x66;&#x61;&#x6e;&#x63;&#x79;&#x63;&#x61;&#x74;</u>&#x2e;';
+  }
+};
